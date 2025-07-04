move scale to affine, add shift for affine, fix bug in 3d scaling

tests/voxelmentations/augmentations/functional/test_volumetrical.py
@@ -88,7 +88,7 @@ def test_pad_CASE_left_AND_replicate_border():
     assert np.allclose(output, expected)
 
 @pytest.mark.functional
-def test_scale_CASE_planar_equal_to_volumetric_AND_axial_dim():
+def test_affine_CASE_planar_equal_to_volumetric_AND_scaling_AND_axial_dim_AND_even_shape_AND_upscaling():
     input = np.expand_dims(
         np.array([
             [ 1,  2,  3,  4],
@@ -99,17 +99,18 @@ def test_scale_CASE_planar_equal_to_volumetric_AND_axial_dim():
         axis=(2, 3),
     )
 
-    scale = 2.
-    volumetric_scale = (scale, scale, scale)
+    scale = 2
+    volumetric_scale = (scale, scale, 1.)
     planar_scale = (scale, scale)
 
     interpolation = E.InterType.NEAREST
     border_mode = E.BorderType.CONSTANT
     fill_value = 0
 
-    volumetric_output = FV.scale(
+    volumetric_output = FV.affine(
         input,
         volumetric_scale,
+        0.,
         interpolation,
         border_mode,
         fill_value,
@@ -129,7 +130,92 @@ def test_scale_CASE_planar_equal_to_volumetric_AND_axial_dim():
     assert np.allclose(volumetric_output, planar_output)
 
 @pytest.mark.functional
-def test_scale_CASE_planar_equal_to_volumetric_AND_twice_isotropic_upscaling():
+def test_affine_CASE_planar_equal_to_volumetric_AND_scaling_AND_axial_dim_AND_even_shape_AND_downscaling():
+    input = np.expand_dims(
+        np.array([
+            [ 1, 1, 5, 5],
+            [ 1, 1, 5, 5],
+            [ 3, 3, 7, 7],
+            [ 3, 3, 7, 7],
+        ]),
+        axis=(2, 3),
+    )
+
+    scale = 0.5
+    volumetric_scale = (scale, scale, 1.)
+    planar_scale = (scale, scale)
+
+    interpolation = E.InterType.NEAREST
+    border_mode = E.BorderType.CONSTANT
+    fill_value = 0
+
+    volumetric_output = FV.affine(
+        input,
+        volumetric_scale,
+        0.,
+        interpolation,
+        border_mode,
+        fill_value,
+    )
+
+    planar_output = FV.plane_affine(
+        input,
+        planar_scale,
+        0.,
+        0.,
+        interpolation,
+        border_mode,
+        fill_value,
+        C.AXIAL_DIM,
+    )
+
+    assert np.allclose(volumetric_output, planar_output)
+
+@pytest.mark.functional
+def test_affine_CASE_planar_equal_to_volumetric_AND_scaling_AND_axial_dim_AND_odd_shape():
+    input = np.expand_dims(
+        np.array([
+            [ 1., 1., 1., 1., 1.],
+            [ 1., 3., 3., 3., 1.],
+            [ 1., 3., 5., 3., 1.],
+            [ 1., 3., 3., 3., 1.],
+            [ 1., 1., 1., 1., 1.],
+        ]),
+        axis=(2, 3),
+    )
+
+    scale = 2.
+    volumetric_scale = (scale, scale, 1.)
+    planar_scale = (scale, scale)
+
+    interpolation = E.InterType.LINEAR
+    border_mode = E.BorderType.CONSTANT
+    fill_value = 0
+
+    volumetric_output = FV.affine(
+        input,
+        volumetric_scale,
+        0.,
+        interpolation,
+        border_mode,
+        fill_value,
+    )
+
+    planar_output = FV.plane_affine(
+        input,
+        planar_scale,
+        0.,
+        0.,
+        interpolation,
+        border_mode,
+        fill_value,
+        C.AXIAL_DIM,
+    )
+
+    assert np.allclose(volumetric_output, planar_output)
+
+@pytest.mark.functional
+def test_affine_CASE_planar_equal_to_volumetric_AND_scaling_AND_twice_isotropic_upscaling():
     input = np.expand_dims(
         np.array([
             [
@@ -149,13 +235,17 @@ def test_scale_CASE_planar_equal_to_volumetric_AND_twice_isotropic_upscaling():
     )
 
     scale = 2.
+    volumetric_scale = (scale, scale, scale)
+    planar_scale = (scale, scale)
+
     interpolation = E.InterType.NEAREST
     border_mode = E.BorderType.CONSTANT
     fill_value = 0
 
-    volumetric_output = FV.scale(
+    volumetric_output = FV.affine(
         input,
-        (scale, scale, scale),
+        volumetric_scale,
+        0.,
         interpolation,
         border_mode,
         fill_value,
@@ -163,7 +253,7 @@ def test_scale_CASE_planar_equal_to_volumetric_AND_twice_isotropic_upscaling():
 
     planar_output = FV.plane_affine(
         input,
-        (scale, scale),
+        planar_scale,
         0.,
         0.,
         interpolation,
@@ -184,6 +274,96 @@ def test_scale_CASE_planar_equal_to_volumetric_AND_twice_isotropic_upscaling():
     )
 
     assert np.allclose(volumetric_output, planar_output)
+
+@pytest.mark.functional
+def test_affine_CASE_planar_equal_to_volumetric_AND_translation_AND_axial_dim():
+    input = np.expand_dims(
+        np.array([
+            [ 1,  2,  3,  4],
+            [ 5,  6,  7,  8],
+            [ 9, 10, 11, 12],
+            [13, 14, 15, 16],
+        ]),
+        axis=(2, 3),
+    )
+
+    scale = 1.
+    volumetric_scale = (scale, scale, scale)
+    planar_scale = (scale, scale)
+
+    xshift = 0.25
+    yshift = 0.25
+    volumetric_shift = (xshift, yshift, 0)
+    planar_shift = (xshift, yshift)
+
+    interpolation = E.InterType.NEAREST
+    border_mode = E.BorderType.CONSTANT
+    fill_value = 0
+
+    volumetric_output = FV.affine(
+        input,
+        volumetric_scale,
+        volumetric_shift,
+        interpolation,
+        border_mode,
+        fill_value,
+    )
+
+    planar_output = FV.plane_affine(
+        input,
+        planar_scale,
+        planar_shift,
+        0.,
+        interpolation,
+        border_mode,
+        fill_value,
+        C.AXIAL_DIM,
+    )
+
+    assert np.allclose(volumetric_output, planar_output)
+
+@pytest.mark.functional
+def test_plane_affine_CASE_twice_isotropic_upscaling():
+    input = np.expand_dims(
+        np.array([
+            [ 1,  2,  3,  4],
+            [ 5,  6,  7,  8],
+            [ 9, 10, 11, 12],
+            [13, 14, 15, 16],
+        ]),
+        axis=(2, 3),
+    )
+
+    expected = np.expand_dims(
+        np.array([
+            [ 6,  6,  7,  7],
+            [ 6,  6,  7,  7],
+            [10, 10, 11, 11],
+            [10, 10, 11, 11],
+        ]),
+        axis=(2, 3),
+    )
+
+    scale = (2., 2.)
+    shift = 0.
+    angle = 0.
+    interpolation = E.InterType.NEAREST
+    border_mode = E.BorderType.CONSTANT
+    fill_value = 0
+    dim = C.AXIAL_DIM
+
+    output = FV.plane_affine(
+        input,
+        scale,
+        shift,
+        angle,
+        interpolation,
+        border_mode,
+        fill_value,
+        dim,
+    )
+
+    assert np.allclose(output, expected)
 
 @pytest.mark.functional
 def test_plane_affine_CASE_90_degree_rotation_AND_square_shape():
@@ -253,49 +433,6 @@ def test_plane_affine_CASE_90_degree_rotation_AND_rectangle_shape():
     scale = (1., 1.)
     shift = 0.
     angle = 90.
-    interpolation = E.InterType.NEAREST
-    border_mode = E.BorderType.CONSTANT
-    fill_value = 0
-    dim = C.AXIAL_DIM
-
-    output = FV.plane_affine(
-        input,
-        scale,
-        shift,
-        angle,
-        interpolation,
-        border_mode,
-        fill_value,
-        dim,
-    )
-
-    assert np.allclose(output, expected)
-
-@pytest.mark.functional
-def test_plane_scale_CASE_twice_isotropic_upscaling():
-    input = np.expand_dims(
-        np.array([
-            [ 1,  2,  3,  4],
-            [ 5,  6,  7,  8],
-            [ 9, 10, 11, 12],
-            [13, 14, 15, 16],
-        ]),
-        axis=(2, 3),
-    )
-
-    expected = np.expand_dims(
-        np.array([
-            [ 6,  6,  7,  7],
-            [ 6,  6,  7,  7],
-            [10, 10, 11, 11],
-            [10, 10, 11, 11],
-        ]),
-        axis=(2, 3),
-    )
-
-    scale = (2., 2.)
-    shift = 0.
-    angle = 0.
     interpolation = E.InterType.NEAREST
     border_mode = E.BorderType.CONSTANT
     fill_value = 0

voxelmentations/augmentations/functional/geometrical.py
@@ -43,10 +43,19 @@ def transpose(points, dims):
 
     return points
 
-def scale(points, scale):
+def affine(points, scale, shift, shape):
     points = np.copy(points)
 
-    M = G.get_volumetric_scaling_matrix(scale)
+    shape = np.array(shape)
+
+    shift = shift * shape
+    point = 0.5 * shape
+
+    K = G.get_volumetric_translation_matrix(point)
+    T = G.get_volumetric_affine_matrix(scale, shift)
+
+    M = K @ T @ np.linalg.inv(K)
+
     np.matmul(points, M.T, out=points)
 
     return points

voxelmentations/augmentations/functional/volumetrical.py
@@ -70,7 +70,7 @@ def transpose(voxel, dims):
 
     return np.require(voxel, requirements=['C_CONTIGUOUS'])
 
-def scale(voxel, scale, interpolation, border_mode, fill_value):
+def affine(voxel, scale, shift, interpolation, border_mode, fill_value):
     """Apply scale transformations to volume
 
         :NOTE:
@@ -84,16 +84,13 @@ def scale(voxel, scale, interpolation, border_mode, fill_value):
             Thus, M = inv(M') and offset = inv(M') @ offset',
             where M and offset are args of affine_transform.
 
-            Some confusions of offset:
-                OpenCV center is calculated by (size - 1) / 2 (float)
-                Scipy center is calculated by (size - 1) // 2 (integer)
-
-            Despite the OpenCV and Scipy transformations are equal for scaling,
-            probably the Scipy offset calculation may has a bug.
+            M has the shape of 3x3.
 
         :args:
-            scale: float
+            scale: (float, float, float)
                 scaling factor in range from 0 to 1
+            shift: float or (float, float, float)
+                translation factor in range from 0 to 1
             interpolation: InterType
                 interpolation mode
             border_mode: BorderType
@@ -104,14 +101,22 @@ def scale(voxel, scale, interpolation, border_mode, fill_value):
     shape = voxel.shape[:C.NUM_SPATIAL_DIMENSIONS]
     shape = np.array(shape)
 
-    T = G.get_volumetric_scaling_matrix(scale)
+    shift = (shape * shift)
+    point = (shape - 1) / 2
 
-    point = np.array([ (ishape - 1) // 2 for ishape in shape ] + [0])
-    offset = np.linalg.inv(T) @ point
+    S = G.get_volumetric_scaling_matrix(scale)
+    invS = np.linalg.inv(S)
+
+    dS = S.diagonal()
+
+    point = np.array([*point, 0])
+    shift = np.array([*shift, 0])
+
+    offset = point - (point + shift) / dS
 
     voxel = sc.ndimage.affine_transform(
         voxel,
-        np.linalg.inv(T),
+        invS,
         offset=offset,
         order=C.MAP_INTER_TO_SC[interpolation],
         mode=C.MAP_BORDER_TYPE_TO_SC[border_mode],
@@ -137,9 +142,9 @@ def plane_affine(voxel, scale, shift, angle, interpolation, border_mode, fill_va
             than plane center in OpenCV has coordinates equal to (height - 1) / 2, (width - 1) / 2.
 
         :args:
-            scale: float
+            scale: (float, float)
                 scaling factor in range from 0 to 1
-            shift: float
+            shift: float or (float, float)
                 translation factor in range from 0 to 1
             angle: float
                 angle of rotation in range from 0 to 180

voxelmentations/augmentations/transformations.py
@@ -243,7 +243,83 @@ class AxialPlaneTranpose(Tranpose):
     _DIMS = (C.AXIAL_DIM, )
 
 @register_as_serializable
-class Scale(DualAugmentation):
+class Affine(DualAugmentation):
+    """Randomly deform a voxel.
+    """
+    def __init__(
+            self,
+            scale_limit=0.05,
+            shift_limit=0.05,
+            border_mode=E.BorderType.DEFAULT,
+            interpolation=E.InterType.DEFAULT,
+            fill_value=0,
+            mask_fill_value=0,
+            always_apply=False,
+            p=0.5,
+        ):
+        """
+            :args:
+                scale_limit: float
+                    limit of scaling
+                shift_limit: float
+                    limit of translation as ratio of size
+                border_mode: BorderType
+                    border mode
+                interpolation: InterType
+                    interpolation mode
+                fill_value: float
+                    padding value of voxel if border_mode is BorderType.CONSTANT
+                mask_fill_value: int or None
+                    padding value if border_mode is BorderType.CONSTANT. if value is None, mask is not affected
+        """
+        super(Affine, self).__init__(always_apply, p)
+
+        self.scale_limit = M.prepare_non_negative_float(scale_limit, 'scale_limit')
+        self.shift_limit = M.prepare_non_negative_float(shift_limit, 'shift_limit')
+
+        self.border_mode = border_mode
+        self.interpolation = interpolation
+        self.mask_interpolation = E.InterType.NEAREST
+
+        self.fill_value = M.prepare_float(fill_value, 'fill_value')
+        self.mask_fill_value = M.prepare_float(mask_fill_value, 'mask_fill_value')
+
+    def get_augmentation_init_args_names(self):
+        return (
+            'scale_limit',
+            'shift_limit',
+            'border_mode',
+            'interpolation',
+            'fill_value',
+            'mask_fill_value'
+        )
+
+    def get_params(self):
+        scale = 1 + (2 * np.random.random() - 1) * self.scale_limit
+        shift = (2 * np.random.random(C.NUM_SPATIAL_DIMENSIONS) - 1) * self.shift_limit
+
+        return {'scale': (scale, scale, scale), 'shift': shift}
+
+    @property
+    def targets_as_params(self):
+        return ['voxel']
+
+    def get_params_dependent_on_targets(self, params):
+        shape = params['voxel'].shape[:C.NUM_SPATIAL_DIMENSIONS]
+
+        return {'shape': shape}
+
+    def apply(self, voxel, scale, shift, **params):
+        return FV.affine(voxel, scale, shift, self.interpolation, self.border_mode, self.fill_value)
+
+    def apply_to_mask(self, mask, scale, shift, **params):
+        return FV.affine(mask, scale, shift, self.mask_interpolation, self.border_mode, self.mask_fill_value)
+
+    def apply_to_points(self, points, scale, shift, shape, **params):
+        return FG.affine(points, scale, shift, shape)
+
+@register_as_serializable
+class Scale(Affine):
     """Randomly scale a voxel.
     """
     def __init__(
@@ -269,16 +345,7 @@ class Scale(DualAugmentation):
                 mask_fill_value: int or None
                     padding value if border_mode is BorderType.CONSTANT. if value is None, mask is not affected
         """
-        super(Scale, self).__init__(always_apply, p)
-
-        self.scale_limit = M.prepare_non_negative_float(scale_limit, 'scale_limit')
-
-        self.border_mode = border_mode
-        self.interpolation = interpolation
-        self.mask_interpolation = E.InterType.NEAREST
-
-        self.fill_value = M.prepare_float(fill_value, 'fill_value')
-        self.mask_fill_value = M.prepare_float(mask_fill_value, 'mask_fill_value')
+        super(Scale, self).__init__(scale_limit, 0, border_mode, interpolation, fill_value, mask_fill_value, always_apply, p)
 
     def get_augmentation_init_args_names(self):
         return (
@@ -288,20 +355,6 @@ class Scale(DualAugmentation):
             'fill_value',
             'mask_fill_value'
         )
-
-    def get_params(self):
-        scale = 1 + (2 * np.random.random() - 1) * self.scale_limit
-
-        return {'scale': (scale, scale, scale)}
-
-    def apply(self, voxel, scale, **params):
-        return FV.scale(voxel, scale, self.interpolation, self.border_mode, self.fill_value)
-
-    def apply_to_mask(self, mask, scale, **params):
-        return FV.scale(mask, scale, self.mask_interpolation, self.border_mode, self.mask_fill_value)
-
-    def apply_to_points(self, points, scale, **params):
-        return FG.scale(points, scale)
 
 @register_as_serializable
 class AxialPlaneAffine(DualAugmentation):