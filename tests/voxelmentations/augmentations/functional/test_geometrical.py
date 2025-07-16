add 3d axial rotation, fix rotation direction in affine

tests/voxelmentations/augmentations/functional/test_geometrical.py
@@ -312,8 +312,8 @@ def test_plane_affine_CASE_90_degree_rotation_AND_square_shape():
     ])
 
     expected = np.array([
-        [0.5, 3.5, 0.5, 1.0],
-        [1.5, 1.5, 0.5, 1.0],
+        [3.5, 0.5, 0.5, 1.0],
+        [2.5, 2.5, 0.5, 1.0],
     ])
 
     scale = (1., 1.)
@@ -397,7 +397,7 @@ def test_plane_affine_CASE_90_degree_rotation_AND_rectangle_shape():
     ])
 
     expected = np.array([
-        [1.5, 1.5, 0.5, 1.0],
+        [2.5, 0.5, 0.5, 1.0],
     ])
 
     scale = (1., 1.)

tests/voxelmentations/augmentations/functional/test_volumetrical.py
@@ -110,6 +110,7 @@ def test_affine_CASE_planar_equal_to_volumetric_AND_scaling_AND_axial_dim_AND_ev
     volumetric_output = FV.affine(
         input,
         volumetric_scale,
+        (0., 0., 0.),
         0.,
         interpolation,
         border_mode,
@@ -153,6 +154,7 @@ def test_affine_CASE_planar_equal_to_volumetric_AND_scaling_AND_axial_dim_AND_ev
     volumetric_output = FV.affine(
         input,
         volumetric_scale,
+        (0., 0., 0.),
         0.,
         interpolation,
         border_mode,
@@ -197,6 +199,7 @@ def test_affine_CASE_planar_equal_to_volumetric_AND_scaling_AND_axial_dim_AND_od
     volumetric_output = FV.affine(
         input,
         volumetric_scale,
+        (0., 0., 0.),
         0.,
         interpolation,
         border_mode,
@@ -248,6 +251,7 @@ def test_affine_CASE_planar_equal_to_volumetric_AND_scaling_AND_twice_isotropic_
     volumetric_output = FV.affine(
         input,
         volumetric_scale,
+        (0., 0., 0.),
         0.,
         interpolation,
         border_mode,
@@ -281,6 +285,53 @@ def test_affine_CASE_planar_equal_to_volumetric_AND_scaling_AND_twice_isotropic_
     assert np.allclose(volumetric_output, planar_output)
 
 @pytest.mark.functional
+def test_affine_CASE_planar_equal_to_volumetric_AND_rotation_AND_axial_dim_AND_even_shape_AND_45degree():
+    input = np.expand_dims(
+        np.array([
+            [ 1,  2,  3,  4],
+            [ 5,  6,  7,  8],
+            [ 9, 10, 11, 12],
+            [13, 14, 15, 16],
+        ]),
+        axis=(2, 3),
+    )
+
+    angle = 90.
+    volumetric_angle = (0., 0., angle)
+    planar_angle = angle
+
+    interpolation = E.InterType.NEAREST
+    border_mode = E.BorderType.CONSTANT
+    fill_value = 0
+
+    volumetric_output = FV.affine(
+        input,
+        (1., 1., 1.),
+        volumetric_angle,
+        0.,
+        interpolation,
+        border_mode,
+        fill_value,
+    )
+
+    planar_output = FV.plane_affine(
+        input,
+        (1., 1.),
+        planar_angle,
+        (0., 0.),
+        0.,
+        interpolation,
+        border_mode,
+        fill_value,
+        C.AXIAL_DIM,
+    )
+
+    print(volumetric_output[..., 0, 0])
+    print(planar_output[..., 0, 0])
+
+    assert np.allclose(volumetric_output, planar_output)
+
+@pytest.mark.functional
 def test_affine_CASE_planar_equal_to_volumetric_AND_translation_AND_axial_dim():
     input = np.expand_dims(
         np.array([
@@ -307,6 +358,7 @@ def test_affine_CASE_planar_equal_to_volumetric_AND_translation_AND_axial_dim():
     volumetric_output = FV.affine(
         input,
         volumetric_scale,
+        (0., 0., 0.),
         volumetric_shift,
         interpolation,
         border_mode,
@@ -386,10 +438,10 @@ def test_plane_affine_CASE_90_degree_rotation_AND_square_shape():
 
     expected = np.expand_dims(
         np.array([
-            [13,  9,  5,  1],
-            [14, 10,  6,  2],
-            [15, 11,  7,  3],
-            [16, 12,  8,  4],
+            [4, 8, 12, 16],
+            [3, 7, 11, 15],
+            [2, 6, 10, 14],
+            [1, 5,  9, 13],
         ]),
         axis=(2, 3),
     )
@@ -431,10 +483,10 @@ def test_plane_affine_CASE_90_degree_rotation_AND_rectangle_shape():
 
     expected = np.expand_dims(
         np.array([
-            [ 0, 0],
-            [ 9, 5],
-            [10, 6],
-            [ 0, 0],
+            [0,  0],
+            [6, 10],
+            [5,  9],
+            [0,  0],
         ]),
         axis=(2, 3),
     )
@@ -521,10 +573,10 @@ def test_plane_affine_CASE_rotation_AND_translation():
 
     expected = np.expand_dims(
         np.array([
-            [ 0,  0,  0, 0 ],
-            [ 0, 13,  9, 5 ],
-            [ 0, 14, 10, 6 ],
-            [ 0, 15, 11, 7 ],
+            [ 0, 0, 0,  0 ],
+            [ 0, 4, 8, 12 ],
+            [ 0, 3, 7, 11 ],
+            [ 0, 2, 6, 10 ],
         ]),
         axis=(2, 3),
     )

voxelmentations/augmentations/functional/geometrical.py
@@ -70,7 +70,7 @@ def plane_affine(points, scale, angle, shear, shift, dim, shape):
     point = 0.5 * shape
 
     K = G.get_planar_translation_matrix(point)
-    T = G.get_planar_affine_matrix(scale, -angle, shear, shift)
+    T = G.get_planar_affine_matrix(scale, angle, shear, shift)
 
     M = K @ T @ np.linalg.inv(K)
 

voxelmentations/augmentations/functional/volumetrical.py
@@ -80,7 +80,7 @@ def transpose(voxel, dims):
 
     return np.require(voxel, requirements=['C_CONTIGUOUS'])
 
-def affine(voxel, scale, shift, interpolation, border_mode, fill_value):
+def affine(voxel, scale, angles, shift, interpolation, border_mode, fill_value):
     """Apply scale transformations to volume
 
         :NOTE:
@@ -107,7 +107,9 @@ def affine(voxel, scale, shift, interpolation, border_mode, fill_value):
         :args:
             scale: (float, float, float)
                 scaling factor in range from 0 to 1
-            shift: float or (float, float, float)
+            angles: (float, float, float)
+                angles to rotate in range from 0 to 180
+            shift: (float, float, float)
                 translation factor in range from 0 to 1
             interpolation: InterType
                 interpolation mode
@@ -123,21 +125,25 @@ def affine(voxel, scale, shift, interpolation, border_mode, fill_value):
     point = (shape - 1) / 2
 
     S = G.get_volumetric_scaling_matrix(scale)
+    R = G.get_volumetric_rotation_matrix(angles)
 
     if len(voxel.shape) == C.NUM_MULTI_CHANNEL_DIMENSIONS:
         point = np.array([*point, 0])
         shift = np.array([*shift, 0])
     else:
         S = S[:C.NUM_SPATIAL_DIMENSIONS, :C.NUM_SPATIAL_DIMENSIONS]
+        R = R[:C.NUM_SPATIAL_DIMENSIONS, :C.NUM_SPATIAL_DIMENSIONS]
 
-    invS = np.linalg.inv(S)
     dS = S.diagonal()
+    invS = np.diag(1 / dS)
 
-    offset = point - (point + shift) / dS
+    invR = R.T
+
+    offset = point - invR @ (point + shift) / dS
 
     voxel = sc.ndimage.affine_transform(
         voxel,
-        invS,
+        invR @ invS,
         offset=offset,
         order=C.MAP_INTER_TO_SC[interpolation],
         mode=C.MAP_BORDER_TYPE_TO_SC[border_mode],
@@ -188,7 +194,7 @@ def plane_affine(voxel, scale, angle, shear, shift, interpolation, border_mode, 
     point = (shape - 1) / 2
 
     K = G.get_planar_translation_matrix(point)
-    T = G.get_planar_affine_matrix(scale, angle, shear, shift)
+    T = G.get_planar_affine_matrix(scale, -angle, shear, shift)
 
     M = K @ T @ np.linalg.inv(K)
     M = M[:2]

voxelmentations/augmentations/utils/geometrical.py
@@ -18,6 +18,44 @@ def get_volumetric_scaling_matrix(scales):
 
     return M
 
+def get_volumetric_rotation_matrix(angles):
+    """Get 4x4 counterclockwise rotation matrix for volumetric transformation
+
+        :args:
+            angles: (float, float, float)
+                angle in degrees
+    """
+    xangle, yangle, zangle = angles
+
+    xangle = np.deg2rad(xangle)
+    yangle = np.deg2rad(yangle)
+    zangle = np.deg2rad(zangle)
+
+    Rx = np.array([
+        [ 1.,             0.,              0., 0. ],
+        [ 0., np.cos(xangle), -np.sin(xangle), 0. ],
+        [ 0., np.sin(xangle),  np.cos(xangle), 0. ],
+        [ 0.,              0.,             0., 1. ],
+    ])
+
+    Ry = np.array([
+        [  np.cos(yangle), 0., np.sin(yangle), 0. ],
+        [              0., 1.,             0., 0. ],
+        [ -np.sin(yangle), 0., np.cos(yangle), 0. ],
+        [              0., 0.,             0., 1. ],
+    ])
+
+    Rz = np.array([
+        [ np.cos(zangle), -np.sin(zangle), 0., 0. ],
+        [ np.sin(zangle),  np.cos(zangle), 0., 0. ],
+        [             0.,              0., 1., 0. ],
+        [             0.,              0., 0., 1. ],
+    ])
+
+    M = Rz @ Ry @ Rx
+
+    return M
+
 def get_volumetric_translation_matrix(shifts):
     """Get 4x4 translation matrix for volumetric transformation
 
@@ -38,10 +76,11 @@ def get_volumetric_translation_matrix(shifts):
 
 def get_volumetric_affine_matrix(scales, angles, shears, shiftes):
     T1 = get_volumetric_scaling_matrix(scales)
+    T2 = get_volumetric_rotation_matrix(angles)
 
     T4 = get_volumetric_translation_matrix(shiftes)
 
-    return T4 @ T1
+    return T4 @ T2 @ T1
 
 def get_planar_scaling_matrix(scales):
     """Get 3x3 scaling matrix for planar transformation
@@ -56,6 +95,23 @@ def get_planar_scaling_matrix(scales):
         [ xscale,     0., 0. ],
         [     0., yscale, 0. ],
         [     0.,     0., 1. ],
+    ])
+
+    return M
+
+def get_planar_rotation_matrix(angle):
+    """Get 3x3 counterclockwise rotation matrix for planar transformation
+
+        :args:
+            angle: float
+                angle in degrees
+    """
+    angle = np.deg2rad(angle)
+
+    M = np.array([
+        [ np.cos(angle), -np.sin(angle), 0. ],
+        [ np.sin(angle),  np.cos(angle), 0. ],
+        [            0.,             0., 1. ],
     ])
 
     return M
@@ -75,23 +131,6 @@ def get_planar_shear_matrix(shears):
         [ 1.            , np.tan(xshear), 0. ],
         [ np.tan(yshear),             1., 0. ],
         [             0.,             0., 1. ],
-    ])
-
-    return M
-
-def get_planar_rotation_matrix(angle):
-    """Get 3x3 clockwise rotation matrix for planar transformation
-
-        :args:
-            angle: float
-                angle in degrees
-    """
-    angle = np.deg2rad(angle)
-
-    M = np.array([
-        [ np.cos(angle), -np.sin(angle), 0. ],
-        [ np.sin(angle),  np.cos(angle), 0. ],
-        [            0.,             0., 1. ],
     ])
 
     return M